d1aa05fcf52f72d9b3e57e6cd7c74c32879e8d08
Gian Del Mundo <gianfranco.delmundo@nix-digital.com>
2021-01-06 17:05:18 +0800

    FIxed Rebase

--- iOS/IntercomWrapper.m
+++ iOS/IntercomWrapper.m
@@ -17,35 +17,8 @@
 RCT_EXPORT_MODULE();
 
 // Available as NativeModules.IntercomWrapper.registerIdentifiedUser
-<<<<<<< HEAD
 RCT_EXPORT_METHOD(registerIdentifiedUser:(NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     NSLog(@"registerIdentifiedUser");
-=======
-RCT_EXPORT_METHOD(initialize:(NSDictionary*)options callback:(RCTResponseSenderBlock)callback) {
-    NSLog(@"initialize with %@", options);
-
-    NSString* apiKey      = options[@"apiKey"];
-    NSString* appId       = options[@"appId"];
-
-    if (apiKey.length > 0 && appId.length > 0) {
-        [Intercom setApiKey:apiKey forAppId:appId];
-
-        #ifdef DEBUG
-            [Intercom enableLogging];
-        #endif
-
-        [Intercom registerUnidentifiedUser];
-        callback(@[[NSNull null]]);
-    } else {
-        NSLog(@"[Intercom] ERROR - No credentials registered. You must supply an apiKey and appId");
-        callback(@[RCTMakeError(@"Error", nil, nil)]);
-    }
-};
-
-// Available as NativeModules.IntercomWrapper.registerIdentifiedUser
-RCT_EXPORT_METHOD(registerIdentifiedUser:(NSDictionary*)options callback:(RCTResponseSenderBlock)callback) {
-    NSLog(@"registerIdentifiedUser with %@", options);
->>>>>>> f5299a1a701eda01ba426018806df66b1a54baa1
 
     NSString* userId      = options[@"userId"];
     NSString* userEmail   = options[@"email"];
